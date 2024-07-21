731adbe77c7908a322f50a0afeffcdec36a22486
David Kapuza <kapuzadavid@gmail.com>
2024-07-21 18:50:08 +0200

    feat: switch to swc compiler

--- apps/api/src/modules/market-data/infrastructure/persistence/relational/entities/asset.entity.ts
+++ apps/api/src/modules/market-data/infrastructure/persistence/relational/entities/asset.entity.ts
@@ -17,19 +17,19 @@ export class AssetEntity extends RelationalEntityHelper implements Asset {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
+  @Column({ type: String })
   name: string;
 
   @Index()
-  @Column()
+  @Column({ type: String })
   class: AssetClassesEnum;
 
   @Index()
-  @Column()
+  @Column({ type: String })
   exchange: AssetExchangesEnum;
 
   @Index()
-  @Column()
+  @Column({ type: String })
   symbol: string;
 
   @CreateDateColumn()
